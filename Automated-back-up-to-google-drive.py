5c51916ec2ab1b6b197e4dcaa6783420a0ea9fbe
vherrero <175484721+herrerovir@users.noreply.github.com>
2024-07-27 19:26:54 +0200

    Add function to create a zip folder

--- Automated-back-up-to-google-drive.py
+++ Automated-back-up-to-google-drive.py
@@ -2,22 +2,47 @@
 
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
+import os, shutil
 
-#Set up authentification with Google Drive API
-gauth = GoogleAuth()
-#Try to load the saved credentials from the first 0Auth approval
-gauth.LoadCredentialsFile("my_credentials.txt")
-#If the app was not authentificated, ask Google for authentification
-if gauth.credentials is None:
-    #Use local default browser for authentification
-    gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    #Refresh credentials if token is expired
-    gauth.Refresh()
-else:
-    #Initialize saved credentials
-    gauth.Authorize()
-#Save current credentials to a text file
-gauth.SaveCredentialsFile("my_credentials.txt")
-#Authorize drive
-drive = GoogleDrive(gauth)
+#Paths
+path_to_zip = r"C:/Users/Backup_Test_Folder"
+path_to_archive_locally = r"C:/Users/Backups_Archive"
+
+#Zip the folder to back up
+def create_zip_folder(path_to_zip, folder_name):
+    """"
+    Function to create a zip folder. 
+    Takes the path to the folder to back up (string) and the folder name (string). 
+    Returns boolean: True if the zip is created, False if it is not.
+    """
+    try:
+        shutil.make_archive(os.path.join(path_to_archive_locally, folder_name), "zip", path_to_zip)
+        return True
+    except FileNotFoundError as e:
+        return False
+
+#Google Drive API authentication
+def google_auth():
+    """
+    Function to authenticate connection to google drive using the API.
+    To avoid authentication every time the script is run, the credentials are saved in 'my_credentials.txt' and called from there
+    to automatically authenticate the access to google drive.
+    Returns: gauth and drive
+    """
+    gauth = GoogleAuth()
+    #Try to load the saved credentials from the first OAuth approval
+    gauth.LoadCredentialsFile("my_credentials.txt")
+    #If the app was not authenticated, it requests authentication from google
+    if gauth.credentials is None:
+        #Use local default browser for authentication
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        #Refresh credentials if token is expired
+        gauth.Refresh()
+    else:
+        #Initialize saved credentials
+        gauth.Authorize()
+    #Save current credentials to a text file
+    gauth.SaveCredentialsFile("my_credentials.txt")
+    drive = GoogleDrive(gauth)
+    return gauth, drive
